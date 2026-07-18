d7654d0b8e16347a16de8787cebb86b2c80c76b5
ARYAN0-WORK <carrer1270@gmail.com>
2026-07-18 13:17:54 +0000

    chore: setting up our server

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import  express  from "express";
+import router from "./routes/index.js";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/",router)
 
 app.get("/health",(req,res)=>{
     res.status(200).json({
