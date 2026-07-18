e82b2ebccb25f949c82ac05351b48ee68395319a
ARYAN0-WORK <carrer1270@gmail.com>
2026-07-18 13:31:12 +0000

    refractor: separate health route and controller

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,11 @@
 import  express  from "express";
-import router from "./routes/index.js";
+import healthRouter from "./routes/health.routes.js";
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/",router)
-
-app.get("/health",(req,res)=>{
-    res.status(200).json({
-        success:true,
-        message:"Server is healty"
-    })
-})
+app.use("/health",healthRouter)
 
 export default app;
 
