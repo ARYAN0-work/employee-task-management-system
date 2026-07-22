0ae166e03ca0ef49e0c72cabcae99edcd1ca733b
ARYAN0-WORK <carrer1270@gmail.com>
2026-07-22 13:32:48 +0000

    feat:adding better error-handling

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,6 +1,15 @@
 import { IUser, User } from "./user.model"
+import { AppError } from "../../errors/AppError";
 
 export const createUser = async (payload: IUser) => {
+  const existingUser = await User.findOne({
+    email: payload.email,
+  });
+
+  if (existingUser) {
+    throw new AppError(409, "Email is already registered.");
+  }
+
   return await User.create(payload);
 };
 
